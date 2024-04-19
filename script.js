272e1192bbde213207aeabb9d962efa5685a9442
andreidan28 <146809875+andreidan28@users.noreply.github.com>
2024-04-19 23:17:00 +0300

    country's flag is displayed

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let searchInput = document.getElementById('searchInput');
 searchBtn.addEventListener('click', function() {
     let searchValue = searchInput.value;
     console.log(searchValue);
-    let url = 'https://restcountries.com/v3.1/name/${searchValue}?fullText=true';
+    let url = `https://restcountries.com/v3.1/name/${searchValue}?fullText=true`;
     console.log(url);
+    fetch(url)  
+    .then((response) => response.json())
+    .then ((data) => {
+        result.innerHTML = `<img src = "${data[0].flags.png}" class="flag-img">`;
+    });
 }); 
