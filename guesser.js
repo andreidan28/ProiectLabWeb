58e5950364708311e1be48acbcc0824543aaa675
andreidan28 <146809875+andreidan28@users.noreply.github.com>
2024-05-17 11:18:04 +0300

    formated files + footer hotfix

--- guesser.js
+++ guesser.js
@@ -1,21 +1,22 @@
-let submitBtn = document.getElementById('submitBtn');
-window.onload = function() {
-    let url = `https://restcountries.com/v3.1/all`;
-    let flag = document.getElementById('flag');
-    let result = document.getElementById('result');
-    fetch(url)
+let submitBtn = document.getElementById("submitBtn");
+window.onload = function () {
+  let url = `https://restcountries.com/v3.1/all`;
+  let flag = document.getElementById("flag");
+  let result = document.getElementById("result");
+  fetch(url)
     .then((response) => response.json())
-    .then ((data) => {
-        let randomCountry = data[Math.floor(Math.random() * data.length)];
-        console.log(randomCountry);
-        flag.innerHTML = `<img src = "${randomCountry.flags.png}" class="flag-img">`;
-        submitBtn.addEventListener('click', function() {
-            let countryInput = document.getElementById('countryInput').value;
-            if (countryInput === randomCountry.name.common) {
-                result.innerHTML = "Correct!";
-            } else {
-                result.innerHTML = "Incorrect! Tara este " + randomCountry.name.common + "!";
-            }
-        });
-    })
-    };
+    .then((data) => {
+      let randomCountry = data[Math.floor(Math.random() * data.length)];
+      console.log(randomCountry);
+      flag.innerHTML = `<img src = "${randomCountry.flags.png}" class="flag-img">`;
+      submitBtn.addEventListener("click", function () {
+        let countryInput = document.getElementById("countryInput").value;
+        if (countryInput === randomCountry.name.common) {
+          result.innerHTML = "Correct!";
+        } else {
+          result.innerHTML =
+            "Incorrect! Tara este " + randomCountry.name.common + "!";
+        }
+      });
+    });
+};
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let searchBtn = document.getElementById('searchBtn');
-let searchInput = document.getElementById('searchInput');
-searchBtn.addEventListener('click', function() {
-    let searchValue = searchInput.value;
-    console.log(searchValue);
-    let url = `https://restcountries.com/v3.1/name/${searchValue}?fullText=true`;
-    console.log(url);
-    fetch(url)  
+let searchBtn = document.getElementById("searchBtn");
+let searchInput = document.getElementById("searchInput");
+searchBtn.addEventListener("click", function () {
+  let searchValue = searchInput.value;
+  console.log(searchValue);
+  let url = `https://restcountries.com/v3.1/name/${searchValue}?fullText=true`;
+  console.log(url);
+  fetch(url)
     .then((response) => response.json())
-    .then ((data) => {
-        console.log(data[0])
-        result.innerHTML = `<img src = "${data[0].flags.png}" class="flag-img">`;
+    .then((data) => {
+      console.log(data[0]);
+      result.innerHTML = `<img src = "${data[0].flags.png}" class="flag-img">`;
     });
 });
